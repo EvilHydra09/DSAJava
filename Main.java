21d7aefc8e0909341e4a45f80b1d850dcd98bbe9
Evil_Hydra <abhidnya09@gmail.com>
2023-10-02 17:44:51 +0530

    Hollow Question

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import Pattern.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Pattern.hollowRectangle(5,6);
+        Pattern.hollowParrellogram(3,5);
     }
 }
--- Pattern/Pattern.java
+++ Pattern/Pattern.java
@@ -96,4 +96,139 @@ public class Pattern {
             System.out.println();
         }
     }
+
+    public static void hollowPyramid(int n){
+        for(int i = 1;i<=n;i++){
+            for(int j = n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                if(i == 1 || i == n|| j==1 || j==i){
+                System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");  // add double space
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void hollowDiamond(int n){
+        for(int i = 1;i<=n;i++){
+            for(int j = n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                if(i == 1 || j==1 || j==i){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");  // add double space
+                }
+            }
+            System.out.println();
+
+        }
+        for(int i = n-1;i>=1;i--){
+            for(int j = n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++ ){
+                if(i == 1 || j==1 || j==i){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");  // add double space
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void heart(int n){
+        for(int i = 2;i<=n;i++){
+            for(int j = n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j = n*2;j>=i*2;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = 1;i<=n*2;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j = n*2;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollowheart(int n){
+        for(int i = 2;i<=n;i++){
+            for(int j = n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                if(i == 1 || j==1 || j==i){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");  // add double space
+                }
+            }
+            for(int j = n*2;j>=i*2;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=i;j++){
+                if(i == 1 || j==1 || j==i){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");  // add double space
+                }
+            }
+            System.out.println();
+        }
+        for(int i = 1;i<=n*2;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j = n*2;j>=i;j--){
+                if( j == n*2||j==1 || j==i){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");  // add double space
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollowParrellogram(int row , int col){
+        for(int i = 1;i<=row;i++){
+            for(int j = col;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=col;j++){
+                if(i == 1|| i == row|| j == 1|| j== col) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
