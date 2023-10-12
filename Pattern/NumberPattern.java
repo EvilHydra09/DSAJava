379c40e85910b9478ab2cfa819ce73b1793fb19f
Evil_Hydra <abhidnya09@gmail.com>
2023-10-12 23:12:53 +0530

    pattern modification

--- Pattern/NumberPattern.java
+++ Pattern/NumberPattern.java
@@ -54,6 +54,26 @@ public class NumberPattern {
                 count++;
             }
             System.out.println();
+//            1
+//            2 3
+//            3 4 5
+//            4 5 6 7
+
+        }
+    }
+    public static void patternNumberRightSeries(int n){
+        int count = 1;
+        for(int i = 1 ;i<=n ;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+//            1
+//            2 3
+//            4 5 6
+//            7 8 9 10
+
         }
     }
 }
